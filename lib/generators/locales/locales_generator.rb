1a19e2fe61c7ee15bc48a9cb63fe852f72ecfd02
Julio Protzek <julioprotzek@gmail.com>
2013-07-29 16:53:05 -0300

    add locales generator

---
+++ lib/generators/locales/locales_generator.rb
@@ -0,0 +1,9 @@
+class LocalesGenerator < Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+
+  def copy_files
+    copy_file 'config/locales/pt-br.yml', 'config/locales/pt-br.yml'
+    copy_file 'config/locales/devise.pt-br.yml', 'config/locales/devise.pt-br.yml'
+    copy_file 'config/locales/simple_form.pt-br.yml', 'config/locales/simple_form.pt-br.yml'
+  end
+end
