89dd1047a880f8bd6abc26e6ff2200300906178e
Julio Protzek <julioprotzek@gmail.com>
2013-10-12 11:15:03 -0300

    config application when addind locales

--- lib/generators/start/locales/locales_generator.rb
+++ lib/generators/start/locales/locales_generator.rb
@@ -1,6 +1,11 @@
 class Start::LocalesGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
 
+  def config_application
+    application "config.time_zone = 'Brasilia'"
+    application "config.i18n.default_locale = 'pt-BR'"
+  end
+
   def copy_files
     copy_file 'config/locales/pt-br.yml', 'config/locales/pt-br.yml'
     copy_file 'config/locales/devise.pt-br.yml', 'config/locales/devise.pt-br.yml'
