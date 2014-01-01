75a8b96ea919e605ec8dc77b48063ecf4087ebbe
Julio Protzek <julio@startae.com.br>
2014-01-01 00:52:57 -0200

    spec for start:heroku_wake_up

---
+++ spec/generators/start/heroku_wake_up/heroku_wake_up_generator_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+require 'generators/start/heroku_wake_up/heroku_wake_up_generator'
+
+describe Start::HerokuWakeUpGenerator do
+  destination File.expand_path("../../../../../tmp", __FILE__)
+
+  before do
+    prepare_destination
+    copy_application
+    copy_development
+    copy_gemfile
+  end
+
+  describe 'the generated files' do
+    before { run_generator %w(start:heroku_wake_up) }
+
+    describe 'initializer' do
+      subject { file('config/initializers/heroku_wake_up.rb') }
+
+      it { should exist }
+    end
+  end
+
+  describe 'Gemfile' do
+    before { run_generator %w(start:heroku_wake_up) }
+    subject { file('Gemfile') }
+
+    it { should contain(/gem "rufus-scheduler", "~> 2.0.19", group: :production/) }
+  end
+
+end
